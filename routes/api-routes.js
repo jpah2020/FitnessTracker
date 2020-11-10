b4b848745fd7dc1e711a1d9af34b740236285d5e
Rusky <jpah.design@gmail.com>
2020-11-09 23:01:10 -0500

    debugging

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,23 +4,19 @@ module.exports = (app) => {
 
     //////Workout Routes//////
     //get all
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}, (err, workouts) => {
-            if(err){
-                console.log(err);
-            } else {
-                res.json(workouts)
-            }
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).populate("exercises").then (workouts => {
+            
+            res.json(workouts)
+            
         });
     });
     //add excerise, set id, push to model, set true
-    app.put("/api/workouts/:workout", ({ params, body }, res) => {
-        db.Workout.findOneAndUpdate({ _id: params.id},
-                                    {$push: {excercises:body }},
-                                    { upsert: true, useFindandModify:false},
-                                    updatedWorkout => {
-                                        res.json(updatedWorkout);
-                                    })
+    app.put("/api/workouts/:workout", async ({ params, body }, res) => {
+        const exercise= await db.Exercise.create(body)
+        const updatedWorkout=await db.Workout.updateOne({ _id: params.workout},
+                                    {$push: {exercises:exercise._id }})
+         res.json(updatedWorkout)                           
     });
     //create new workout
     app.post('/api/workouts', (req,res) => {
